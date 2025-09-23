0317955db7efd2484c82a10d987b2b6e791ba7d2
BBoonZ <bosszaza0380@gmail.com>
2025-09-23 17:56:25 +0700

    Update tripBudget + Team, Iframe Edit, Share

--- template/static/javascript/tripPlan.js
+++ template/static/javascript/tripPlan.js
@@ -214,3 +214,13 @@ function editDeleteActivityDetails(btn) {
         text.style.display = "none";
     }
 }
+
+function previewImage(event) {
+    const file = event.target.files[0];
+    const preview = document.getElementById('preview');
+    
+    if (file) {
+      preview.src = URL.createObjectURL(file);
+      preview.style.display = "block";
+    }
+  }
